2c602e85d063a51482a8f513f2f6ac5a082e0774
zezang <zezang@gmail.com>
2022-07-31 22:54:08 -0700

    set up template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+        <div>
+            <h1>React-Template</h1>
+            <button onClick ={() => alert('CLICKED')}>TEST BUTTON</button>
+        </div>
+        
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/index.js'),
+    output: {
+        path: path.resolve(__dirname, '/dist'),
+        filename: 'bundle.js'
+      },
+
+    module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+            }],
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: './src/index.html',
+          filename: './index.html',
+        }),
+    ],
+    devServer: {
+      static: path.resolve(__dirname, './dist'),
+    }
+  };
